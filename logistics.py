7b8fbb82370cb20bd233662e7591e85be5601fac
Kiran Pesarlanka <kiranpesarlanka9@gmail.com>
2020-02-19 15:27:58 +0530

    Update logistics.py

--- logistics.py
+++ logistics.py
@@ -69,6 +69,4 @@ def bookings():
         return get_bookings()
     if request.method == 'POST':
         return create_booking(request.json)
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+
